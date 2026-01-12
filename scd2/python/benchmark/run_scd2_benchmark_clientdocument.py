730d9095e033fa4dba459dd8b604a99fcd14547f
Guido Schmutz <schmutz68@gmail.com>
2026-01-12 12:11:22 +0100

    Update run_scd2_benchmark_clientdocument.py

--- scd2/python/benchmark/run_scd2_benchmark_clientdocument.py
+++ scd2/python/benchmark/run_scd2_benchmark_clientdocument.py
@@ -269,7 +269,7 @@ def run_merge_all(tshirt: str, run_id: str, case_id: int, case_description: str,
     conn = get_trino_connection()
     create_dim_table(conn, TRINO_CATALOG, TRINO_SCHEMA, f"dim_{table_name}_{case_id}_{tshirt}", s3_warehouse_bucket=S3_WAREHOUSE_BUCKET, s3_warehouse_prefix=S3_WAREHOUSE_PREFIX, pk_col_with_type=f"clientdocumentid BIGINT", cols_with_type=cols_with_type, partition_cols=partition_cols, sort_cols=sort_cols)
 
-    start_ts = datetime(2025, 10, 12, 0, 0, 0)
+    start_ts = datetime(2025, 10, 13, 0, 0, 0)
     for day in range(number_of_days):
         load_date = start_ts + timedelta(days=day)
         logger.info(f"Processing load date: {load_date}")
