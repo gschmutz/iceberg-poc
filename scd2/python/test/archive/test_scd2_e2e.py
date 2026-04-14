365032b7bd633d9105fdff0ea28f43c1d62c73be
Guido Schmutz <schmutz68@gmail.com>
2026-04-14 16:23:10 +0200

    update

--- scd2/python/test/archive/test_scd2_e2e.py
+++ scd2/python/test/archive/test_scd2_e2e.py
@@ -1,51 +1,65 @@
-import sys
+import logging
 import os
-import logging
-from datetime import date, timedelta, datetime
-import logging
+import sys
+from datetime import date, datetime, timedelta
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../lib')))
-from util import get_param, get_credential, replace_vars_in_string, render_init, render_data
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../lib')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../lib")))
+from util import (
+    get_credential,
+    get_param,
+    render_data,
+    render_init,
+    replace_vars_in_string,
+)
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../lib")))
+from commons import (
+    COLS_WITH_TYPE,
+    EXCLUDE_COLS,
+    RAW_TABLE_NAME,
+    S3_WAREHOUSE_BUCKET,
+    S3_WAREHOUSE_PREFIX,
+    SCD2_VIEW_NAME,
+    TRINO_CATALOG,
+    TRINO_SCHEMA,
+    create_dim_table_for_test,
+    create_raw_table,
+    scd2_merge_as_test,
+)
 from constants import MAX_TS
-from commons import TRINO_CATALOG, TRINO_SCHEMA, S3_WAREHOUSE_BUCKET, S3_WAREHOUSE_PREFIX, DIM_TABLE_NAME, RAW_TABLE_NAME, SCD2_VIEW_NAME, EXCLUDE_COLS, COLS_WITH_TYPE, create_dim_table_for_test, scd2_merge_as_test, create_raw_table
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-FILE_NAME="reports/scd2_e2e.md"
+FILE_NAME = "reports/scd2_e2e.md"
 
-load_ts_1= datetime.strptime('2026-01-01 00:00:00', '%Y-%m-%d %H:%M:%S')
-current_ts_1 = datetime.strptime('2026-01-02 00:00:00', '%Y-%m-%d %H:%M:%S')
+load_ts_1 = datetime.strptime("2026-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")
+current_ts_1 = datetime.strptime("2026-01-02 00:00:00", "%Y-%m-%d %H:%M:%S")
 
-load_ts_2 = datetime.strptime('2026-01-05 00:00:00', '%Y-%m-%d %H:%M:%S')
-current_ts_2 = datetime.strptime('2026-01-06 00:00:00', '%Y-%m-%d %H:%M:%S')
+load_ts_2 = datetime.strptime("2026-01-05 00:00:00", "%Y-%m-%d %H:%M:%S")
+current_ts_2 = datetime.strptime("2026-01-06 00:00:00", "%Y-%m-%d %H:%M:%S")
 
-load_ts_3 = datetime.strptime('2026-01-15 00:00:00', '%Y-%m-%d %H:%M:%S')
-current_ts_3 = datetime.strptime('2026-01-16 00:00:00', '%Y-%m-%d %H:%M:%S')
+load_ts_3 = datetime.strptime("2026-01-15 00:00:00", "%Y-%m-%d %H:%M:%S")
+current_ts_3 = datetime.strptime("2026-01-16 00:00:00", "%Y-%m-%d %H:%M:%S")
 
-load_ts_4 = datetime.strptime('2026-02-10 00:00:00', '%Y-%m-%d %H:%M:%S')
-current_ts_4 = datetime.strptime('2026-02-11 00:00:00', '%Y-%m-%d %H:%M:%S')
+load_ts_4 = datetime.strptime("2026-02-10 00:00:00", "%Y-%m-%d %H:%M:%S")
+current_ts_4 = datetime.strptime("2026-02-11 00:00:00", "%Y-%m-%d %H:%M:%S")
 
-load_ts_5 = datetime.strptime('2026-02-20 00:00:00', '%Y-%m-%d %H:%M:%S')
-current_ts_5 = datetime.strptime('2026-02-21 00:00:00', '%Y-%m-%d %H:%M:%S')
-
-
-#@pytest.fixture(autouse=True, scope="session")
-#def setup_data(request):
-#    create_raw_table()
-#    create_dim_table(conn, TRINO_CATALOG, TRINO_SCHEMA, "{DIM_TABLE_NAME}", s3_warehouse_bucket=S3_WAREHOUSE_BUCKET, s3_warehouse_prefix=S3_WAREHOUSE_PREFIX, pk_col_with_type="id INT", cols_with_type=cols_with_type, partition_cols=["dp_ts_from"], sort_cols=[])
-#    yield
-#    logger.info("Finished all tests")
+load_ts_5 = datetime.strptime("2026-02-20 00:00:00", "%Y-%m-%d %H:%M:%S")
+current_ts_5 = datetime.strptime("2026-02-21 00:00:00", "%Y-%m-%d %H:%M:%S")
 
 
 def test_step_1(ctx):
-    logger.info("-------------------------------- Test Step 1 --------------------------------")
+    logger.info(
+        "-------------------------------- Test Step 1 --------------------------------"
+    )
 
     create_raw_table(ctx)
     create_dim_table_for_test(ctx)
     render_init("End-to-End SCD2 Test Case over multiple days", FILE_NAME)
-    render_data("This test performs SCD2 operations over 5 days.", output_file_name=FILE_NAME)
+    render_data(
+        "This test performs SCD2 operations over 5 days.", output_file_name=FILE_NAME
+    )
 
     test_description = "Insert 3 records into raw table and perform initial SCD2 merge."
 
@@ -71,27 +85,67 @@ def test_step_1(ctx):
     """
 
     expected = [
-        (1, "Alice", "Meyer", "Zurich", "alice.meyer@example.com",
-        load_ts_1, MAX_TS, True, True,
-        current_ts_1, MAX_TS,
-        "00B9A7122065F01BE7FD23C6FB962AEE6DE3B84D0BA50409DC26FC5A150FBDC8"),
-
-        (2, "Bob", "Keller", "Bern", "bob.keller@example.com",
-        load_ts_1, MAX_TS, True, True,
-        current_ts_1, MAX_TS,
-        "D28A23C8422275E006FCF3D86AA51CF4E058FB495B8E48560FC9BF7BCC019B40"),
-
-        (3, "Clara", "Schmid", "Basel", "clara.schmid@example.com",
-        load_ts_1, MAX_TS, True, True,
-        current_ts_1, MAX_TS,
-        "77C069EE2AA3730894A6E3319ADC455C203B6CC4D35B0B912C2FAADF3C687676"),
+        (
+            1,
+            "Alice",
+            "Meyer",
+            "Zurich",
+            "alice.meyer@example.com",
+            load_ts_1,
+            MAX_TS,
+            True,
+            True,
+            current_ts_1,
+            MAX_TS,
+            "00B9A7122065F01BE7FD23C6FB962AEE6DE3B84D0BA50409DC26FC5A150FBDC8",
+        ),
+        (
+            2,
+            "Bob",
+            "Keller",
+            "Bern",
+            "bob.keller@example.com",
+            load_ts_1,
+            MAX_TS,
+            True,
+            True,
+            current_ts_1,
+            MAX_TS,
+            "D28A23C8422275E006FCF3D86AA51CF4E058FB495B8E48560FC9BF7BCC019B40",
+        ),
+        (
+            3,
+            "Clara",
+            "Schmid",
+            "Basel",
+            "clara.schmid@example.com",
+            load_ts_1,
+            MAX_TS,
+            True,
+            True,
+            current_ts_1,
+            MAX_TS,
+            "77C069EE2AA3730894A6E3319ADC455C203B6CC4D35B0B912C2FAADF3C687676",
+        ),
     ]
 
     # run test
-    scd2_merge_as_test(ctx, test_step=1, ins_stmt=insert_sql_1, load_ts=load_ts_1, current_ts=current_ts_1, expected=expected, output_file_name=FILE_NAME, test_description=test_description)
+    scd2_merge_as_test(
+        ctx,
+        test_step=1,
+        ins_stmt=insert_sql_1,
+        load_ts=load_ts_1,
+        current_ts=current_ts_1,
+        expected=expected,
+        output_file_name=FILE_NAME,
+        test_description=test_description,
+    )
+
 
 def test_step_2(ctx):
-    logger.info("-------------------------------- Test Step 2 --------------------------------")
+    logger.info(
+        "-------------------------------- Test Step 2 --------------------------------"
+    )
 
     cursor = ctx.conn.cursor()
 
@@ -121,27 +175,67 @@ def test_step_2(ctx):
     """
 
     expected = [
-        (1, "Alice", "Meyer", "Zurich", "alice.meyer@example.com",
-        load_ts_1, MAX_TS, True, True,
-        current_ts_1, MAX_TS,
-        "00B9A7122065F01BE7FD23C6FB962AEE6DE3B84D0BA50409DC26FC5A150FBDC8"),
-
-        (2, "Bob", "Keller", "Bern", "bob.keller@example.com",
-        load_ts_1, MAX_TS, True, True,
-        current_ts_1, MAX_TS,
-        "D28A23C8422275E006FCF3D86AA51CF4E058FB495B8E48560FC9BF7BCC019B40"),
-
-        (3, "Clara", "Schmid", "Basel", "clara.schmid@example.com",
-        load_ts_1, load_ts_2 - timedelta(seconds=1), False, True,
-        current_ts_1, current_ts_2,
-        "77C069EE2AA3730894A6E3319ADC455C203B6CC4D35B0B912C2FAADF3C687676"),
+        (
+            1,
+            "Alice",
+            "Meyer",
+            "Zurich",
+            "alice.meyer@example.com",
+            load_ts_1,
+            MAX_TS,
+            True,
+            True,
+            current_ts_1,
+            MAX_TS,
+            "00B9A7122065F01BE7FD23C6FB962AEE6DE3B84D0BA50409DC26FC5A150FBDC8",
+        ),
+        (
+            2,
+            "Bob",
+            "Keller",
+            "Bern",
+            "bob.keller@example.com",
+            load_ts_1,
+            MAX_TS,
+            True,
+            True,
+            current_ts_1,
+            MAX_TS,
+            "D28A23C8422275E006FCF3D86AA51CF4E058FB495B8E48560FC9BF7BCC019B40",
+        ),
+        (
+            3,
+            "Clara",
+            "Schmid",
+            "Basel",
+            "clara.schmid@example.com",
+            load_ts_1,
+            load_ts_2 - timedelta(seconds=1),
+            False,
+            True,
+            current_ts_1,
+            current_ts_2,
+            "77C069EE2AA3730894A6E3319ADC455C203B6CC4D35B0B912C2FAADF3C687676",
+        ),
     ]
 
     # run test
-    scd2_merge_as_test(ctx, test_step=2, ins_stmt=insert_sql_2, load_ts=load_ts_2, current_ts=current_ts_2, expected=expected, output_file_name=FILE_NAME, test_description=test_description)
+    scd2_merge_as_test(
+        ctx,
+        test_step=2,
+        ins_stmt=insert_sql_2,
+        load_ts=load_ts_2,
+        current_ts=current_ts_2,
+        expected=expected,
+        output_file_name=FILE_NAME,
+        test_description=test_description,
+    )
+
 
 def test_step_3(ctx):
-    logger.info("-------------------------------- Test Step 3 --------------------------------")
+    logger.info(
+        "-------------------------------- Test Step 3 --------------------------------"
+    )
 
     cursor = ctx.conn.cursor()
 
@@ -171,27 +265,67 @@ def test_step_3(ctx):
     """
 
     expected = [
-        (1, "Alice", "Meyer", "Zurich", "alice.meyer@example.com",
-        load_ts_1, MAX_TS, True, True,
-        current_ts_1, MAX_TS,
-        "00B9A7122065F01BE7FD23C6FB962AEE6DE3B84D0BA50409DC26FC5A150FBDC8"),
-
-        (2, "Bob", "Keller", "Bern", "bob.keller@example.com",
-        load_ts_1, MAX_TS, True, True,
-        current_ts_1, MAX_TS,
-        "D28A23C8422275E006FCF3D86AA51CF4E058FB495B8E48560FC9BF7BCC019B40"),
-
-        (3, "Clara", "Schmid", "Basel", "clara.schmid@example.com",
-        load_ts_1, load_ts_2 - timedelta(seconds=1), False, True,
-        current_ts_1, current_ts_2,
-        "77C069EE2AA3730894A6E3319ADC455C203B6CC4D35B0B912C2FAADF3C687676"),
+        (
+            1,
+            "Alice",
+            "Meyer",
+            "Zurich",
+            "alice.meyer@example.com",
+            load_ts_1,
+            MAX_TS,
+            True,
+            True,
+            current_ts_1,
+            MAX_TS,
+            "00B9A7122065F01BE7FD23C6FB962AEE6DE3B84D0BA50409DC26FC5A150FBDC8",
+        ),
+        (
+            2,
+            "Bob",
+            "Keller",
+            "Bern",
+            "bob.keller@example.com",
+            load_ts_1,
+            MAX_TS,
+            True,
+            True,
+            current_ts_1,
+            MAX_TS,
+            "D28A23C8422275E006FCF3D86AA51CF4E058FB495B8E48560FC9BF7BCC019B40",
+        ),
+        (
+            3,
+            "Clara",
+            "Schmid",
+            "Basel",
+            "clara.schmid@example.com",
+            load_ts_1,
+            load_ts_2 - timedelta(seconds=1),
+            False,
+            True,
+            current_ts_1,
+            current_ts_2,
+            "77C069EE2AA3730894A6E3319ADC455C203B6CC4D35B0B912C2FAADF3C687676",
+        ),
     ]
 
     # run test
-    scd2_merge_as_test(ctx, test_step=3, ins_stmt=insert_sql_3, load_ts=load_ts_3, current_ts=current_ts_3, expected=expected, output_file_name=FILE_NAME, test_description=test_description)
+    scd2_merge_as_test(
+        ctx,
+        test_step=3,
+        ins_stmt=insert_sql_3,
+        load_ts=load_ts_3,
+        current_ts=current_ts_3,
+        expected=expected,
+        output_file_name=FILE_NAME,
+        test_description=test_description,
+    )
+
 
 def test_step_4(ctx):
-    logger.info("-------------------------------- Test Step 4 --------------------------------")
+    logger.info(
+        "-------------------------------- Test Step 4 --------------------------------"
+    )
 
     cursor = ctx.conn.cursor()
 
@@ -221,28 +355,67 @@ def test_step_4(ctx):
     """
 
     expected = [
-        (1, "Alice", "Meyer", "Zurich", "alice.meyer@example.com",
-        load_ts_1, MAX_TS, True, True,
-        current_ts_1, MAX_TS,
-        "00B9A7122065F01BE7FD23C6FB962AEE6DE3B84D0BA50409DC26FC5A150FBDC8"),
-
-        (2, "Bob", "Keller", "Bern", "bob.keller@example.com",
-        load_ts_1, MAX_TS, True, True,
-        current_ts_1, MAX_TS,
-        "D28A23C8422275E006FCF3D86AA51CF4E058FB495B8E48560FC9BF7BCC019B40"),
-
-        (3, "Clara", "Schmid", "Basel", "clara.schmid@example.com",
-        load_ts_1, load_ts_2 - timedelta(seconds=1), False, True,
-        current_ts_1, current_ts_2,
-        "77C069EE2AA3730894A6E3319ADC455C203B6CC4D35B0B912C2FAADF3C687676"),
+        (
+            1,
+            "Alice",
+            "Meyer",
+            "Zurich",
+            "alice.meyer@example.com",
+            load_ts_1,
+            MAX_TS,
+            True,
+            True,
+            current_ts_1,
+            MAX_TS,
+            "00B9A7122065F01BE7FD23C6FB962AEE6DE3B84D0BA50409DC26FC5A150FBDC8",
+        ),
+        (
+            2,
+            "Bob",
+            "Keller",
+            "Bern",
+            "bob.keller@example.com",
+            load_ts_1,
+            MAX_TS,
+            True,
+            True,
+            current_ts_1,
+            MAX_TS,
+            "D28A23C8422275E006FCF3D86AA51CF4E058FB495B8E48560FC9BF7BCC019B40",
+        ),
+        (
+            3,
+            "Clara",
+            "Schmid",
+            "Basel",
+            "clara.schmid@example.com",
+            load_ts_1,
+            load_ts_2 - timedelta(seconds=1),
+            False,
+            True,
+            current_ts_1,
+            current_ts_2,
+            "77C069EE2AA3730894A6E3319ADC455C203B6CC4D35B0B912C2FAADF3C687676",
+        ),
     ]
 
     # run test
-    scd2_merge_as_test(ctx, test_step=4, ins_stmt=insert_sql_4, load_ts=load_ts_4, current_ts=current_ts_4, expected=expected, output_file_name=FILE_NAME, test_description=test_description)
+    scd2_merge_as_test(
+        ctx,
+        test_step=4,
+        ins_stmt=insert_sql_4,
+        load_ts=load_ts_4,
+        current_ts=current_ts_4,
+        expected=expected,
+        output_file_name=FILE_NAME,
+        test_description=test_description,
+    )
 
 
 def test_step_5(ctx):
-    logger.info("-------------------------------- Test Step 5 --------------------------------")
+    logger.info(
+        "-------------------------------- Test Step 5 --------------------------------"
+    )
 
     cursor = ctx.conn.cursor()
 
@@ -273,27 +446,58 @@ def test_step_5(ctx):
     """
 
     expected = [
-        (1, "Alice", "Meyer", "Zurich", "alice.meyer@example.com",
-        load_ts_1, MAX_TS, True, True,
-        current_ts_1, MAX_TS,
-        "00B9A7122065F01BE7FD23C6FB962AEE6DE3B84D0BA50409DC26FC5A150FBDC8"),
-
-        (2, "Bob", "Keller", "Bern", "bob.keller@example.com",
-        load_ts_1, MAX_TS, True, True,
-        current_ts_1, MAX_TS,
-        "D28A23C8422275E006FCF3D86AA51CF4E058FB495B8E48560FC9BF7BCC019B40"),
-
-        (3, "Clara", "Schmid", "Basel", "clara.schmid@example.com",
-        load_ts_1, load_ts_2 - timedelta(seconds=1), False, True,
-        current_ts_1, current_ts_2,
-        "77C069EE2AA3730894A6E3319ADC455C203B6CC4D35B0B912C2FAADF3C687676"),
+        (
+            1,
+            "Alice",
+            "Meyer",
+            "Zurich",
+            "alice.meyer@example.com",
+            load_ts_1,
+            MAX_TS,
+            True,
+            True,
+            current_ts_1,
+            MAX_TS,
+            "00B9A7122065F01BE7FD23C6FB962AEE6DE3B84D0BA50409DC26FC5A150FBDC8",
+        ),
+        (
+            2,
+            "Bob",
+            "Keller",
+            "Bern",
+            "bob.keller@example.com",
+            load_ts_1,
+            MAX_TS,
+            True,
+            True,
+            current_ts_1,
+            MAX_TS,
+            "D28A23C8422275E006FCF3D86AA51CF4E058FB495B8E48560FC9BF7BCC019B40",
+        ),
+        (
+            3,
+            "Clara",
+            "Schmid",
+            "Basel",
+            "clara.schmid@example.com",
+            load_ts_1,
+            load_ts_2 - timedelta(seconds=1),
+            False,
+            True,
+            current_ts_1,
+            current_ts_2,
+            "77C069EE2AA3730894A6E3319ADC455C203B6CC4D35B0B912C2FAADF3C687676",
+        ),
     ]
 
     # run test
-    scd2_merge_as_test(ctx, test_step=5, ins_stmt=insert_sql_5, load_ts=load_ts_5, current_ts=current_ts_5, expected=expected, output_file_name=FILE_NAME, test_description=test_description)
-
-
-
-
-
-
+    scd2_merge_as_test(
+        ctx,
+        test_step=5,
+        ins_stmt=insert_sql_5,
+        load_ts=load_ts_5,
+        current_ts=current_ts_5,
+        expected=expected,
+        output_file_name=FILE_NAME,
+        test_description=test_description,
+    )
